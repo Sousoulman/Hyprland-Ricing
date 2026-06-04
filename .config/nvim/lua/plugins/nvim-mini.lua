af63309608be57950674c1f645fb4bd80d556bfd
Sousoulman <lucadulieu@gmail.com>
2026-06-04 12:29:54 +0200

    make the conf to lua change for hyprland

--- .config/nvim/lua/plugins/nvim-mini.lua
+++ .config/nvim/lua/plugins/nvim-mini.lua
@@ -2,10 +2,10 @@ return {
 	"nvim-mini/mini.nvim",
 	version = false,
 	config = function()
-		require("mini.pairs").setup({})
-        require("mini.completion").setup({})
-        require("mini.git").setup({})
-        require("mini.icons").setup({})
-        require("mini.starter").setup({})
+		require("mini.pairs").setup()
+        require("mini.completion").setup()
+        require("mini.git").setup()
+        require("mini.icons").setup()
+        require("mini.starter").setup()
     end,
 }
